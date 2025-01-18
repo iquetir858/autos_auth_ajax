9f61798bda023677f94ba1bcaefb0e2c6f0ed68d
Inma Quesada Tirado <iquetir858@g.educaand.es>
2025-01-18 19:52:12 +0100

    client pt1

--- client.php
+++ client.php
@@ -1,5 +1,25 @@
 <?php
 
-class client{
+class client
+{
     //constructor que conecta con el server
+    public function __construct()
+    {
+        $params = array(
+            'location' => 'http://localhost/autos_auth_ajax/server.php',
+            'uri' => 'http://localhost/autos_auth_ajax/server.php',
+            'trace' => 1
+        );
+
+        $this->instance = new SoapClient(NULL, $params);
+
+        //Establecer las cabeceras
+
+    }
+
+    /* Funciones a crear
+    ObtenerMarcas(?)
+    ObtenerMarcasUrl()
+    ObtenerModelosPorMarca($marca)
+    */
 }
